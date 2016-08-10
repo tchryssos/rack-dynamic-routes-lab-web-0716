1948d19b5045d649b4713aeea7c741b0585987c8
Troy Chryssos <github email address>
2016-08-10 17:59:19 -0400

    Updated Application to allow for item searches and status changes.

---
+++ app/application.rb
@@ -0,0 +1,29 @@
+# require_relative "item.rb"
+require 'pry'
+
+class Application
+
+  #@@items
+
+  def call (env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name=req.path.split('/items/').last
+      item=@@items.find {|item| item.name==item_name}
+      # binding.pry
+      if item
+        resp.write item.price
+      else
+        resp.status=400
+        resp.write "Item not found"
+      end
+    else
+      resp.write "Route not found"
+      resp.status=404
+    end
+    resp.finish
+  end
+
+end
